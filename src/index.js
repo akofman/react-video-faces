c059a695ef8e0769155da55df64e50382754371f
Alexis Kofman <alexis.kofman@gmail.com>
2018-07-05 14:19:50 +0200

    optim with raf

--- src/index.js
+++ src/index.js
@@ -2,101 +2,110 @@ import React, { Component } from 'react';
 import { toNetInput, loadFaceDetectionModel, locateFaces, drawDetection } from 'face-api.js/dist/face-api';
 
 class VideoFaces extends Component {
-    constructor() {
-        super();
-        this.state = {
-            areFaceModelsLoaded: false
-        };
+  constructor() {
+    super();
+    this.state = {
+      areFaceModelsLoaded: false
+    };
+  }
+
+  async componentDidMount() {
+    if (!this.faceModel) this.faceModel = await loadFaceDetectionModel(this.props.models);
+
+    this.rafInterval = 1000 / this.props.fps;
+    this.rafThen = Date.now();
+
+    this.setState({
+      areFaceModelsLoaded: true
+    });
+  }
+
+  componentWillUnmount() {
+    cancelAnimationFrame(this.raf);
+  }
+
+  analyseVideoFrames = () => {
+    this.raf = requestAnimationFrame(this.analyseVideoFrames);
+    const now = Date.now();
+    const delta = now - this.rafThen;
+    if (delta > this.rafInterval) {
+      this.rafThen = now - (delta % this.rafInterval);
+      this.detectFaces();
     }
+  }
 
+  detectFaces = async () => {
+    if (this.props.drawDetection) this.canvasContext.clearRect(0, 0, this.videoElement.videoWidth, this.videoElement.videoHeight);
 
-    async componentDidMount() {
-        if (!this.faceModel) this.faceModel = await loadFaceDetectionModel(this.props.models);
-        this.setState({
-            areFaceModelsLoaded: true
-        });
+    const input = await toNetInput(this.videoElement);
+    const result = await locateFaces(input);
+    this.onResult(result);
+  }
+
+  onResult = (result) => {
+    if (this.props.drawDetection) {
+      drawDetection(this.canvasElement, result.map(det => det.forSize(this.videoElement.videoWidth, this.videoElement.videoHeight)))
     }
+    this.props.onResult && this.props.onResult(result);
+  }
 
-    componentWillUnmount() {
-        clearTimeout(this.timeout);
-        cancelAnimationFrame(this.raf);
+
+  onPlaying = () => {
+    if (this.state.areFaceModelsLoaded) {
+      this.analyseVideoFrames();
     }
+    if (typeof this.props.onPlaying === 'function') this.props.onPlaying();
+  }
 
-    detectFaces = async () => {
-        if (this.canvasContext) this.canvasContext.clearRect(0, 0, this.videoElement.videoWidth, this.videoElement.videoHeight);
+  onPause = () => {
+    this.raf = cancelAnimationFrame(this.raf);
+    if (this.canvasContext) this.canvasContext.clearRect(0, 0, this.videoElement.videoWidth, this.videoElement.videoHeight);
+    if (typeof this.props.onPause === 'function') this.props.onPause();
+  }
 
-        const input = await toNetInput(this.videoElement);
-        const result = await locateFaces(input);
-        this.onFacesDetection(result);
+  onLoadedData = () => {
+    if (this.props.drawDetection) {
+      this.canvasElement.width = this.videoElement.videoWidth;
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.style.left = `${this.videoElement.offsetLeft}px`;
+      this.canvasElement.style.top = `${this.videoElement.offsetTop}px`;
+      this.canvasElement.style.pointerEvents = 'none';
+      this.canvasContext = this.canvasElement.getContext('2d');
     }
+    if (typeof this.props.onLoadedData === 'function') this.props.onLoadedData();
+  }
 
-    onFacesDetection = (result) => {
-        if (this.props.drawDetection) {                
-            drawDetection(this.canvasElement, result.map(det => det.forSize(this.videoElement.videoWidth, this.videoElement.videoHeight)))
+  render() {
+    const { src, onPause, onPlaying, onLoadedData, onResult, drawDetection, ...options } = this.props;
+
+    return (
+      <div style={{ "position": "relative" }}>
+        {
+          this.state.areFaceModelsLoaded && <video
+            {...options}
+            onPlaying={this.onPlaying}
+            onPause={this.onPause}
+            onLoadedData={this.onLoadedData}
+            ref={refs => {
+              this.videoElement = refs;
+            }}
+          >
+            <source src={src} type="video/mp4" />
+
+          </video>
         }
-        this.props.onResult && this.props.onResult(result);
-    }
-
-    onPlaying = (eventListener) => {
-        if (this.state.areFaceModelsLoaded) {
-            this.timeout = setTimeout(() => {
-                this.raf = requestAnimationFrame(() => this.onPlaying(eventListener));
-                this.detectFaces();
-            }, 1000 / this.props.fps);
+        {
+          drawDetection && <canvas
+            style={{ "position": "absolute" }}
+            hidden={false}
+            ref={refs => {
+              this.canvasElement = refs;
+            }}
+          />
         }
-        if (typeof eventListener === 'function') eventListener();
-    }
-
-    onPause = (eventListener) => {
-        this.timeout = clearTimeout(this.timeout);
-        this.raf = cancelAnimationFrame(this.raf);
-        if (this.canvasContext) this.canvasContext.clearRect(0, 0, this.videoElement.videoWidth, this.videoElement.videoHeight);
-        if (typeof eventListener === 'function') eventListener();
-    }
-
-    onLoadedData = (eventListener) => {
-        if (this.props.drawDetection) {
-            this.canvasElement.width = this.videoElement.videoWidth;
-            this.canvasElement.height = this.videoElement.videoHeight;
-            this.canvasElement.style.left = `${this.videoElement.offsetLeft}px`;
-            this.canvasElement.style.top = `${this.videoElement.offsetTop}px`;
-            this.canvasElement.style.pointerEvents = 'none';
-            this.canvasContext = this.canvasElement.getContext('2d');
-        }
-        if (typeof eventListener === 'function') eventListener();
-    }
-
-    render() {
-        const { src, onPause, onPlaying, onLoadedData, onResult, drawDetection, ...options } = this.props;
-
-        return (
-            <div style={{ "position": "relative" }}>
-                {
-                    this.state.areFaceModelsLoaded && <video
-                        {...options}
-                        onPlaying={() => this.onPlaying(onPlaying)}
-                        onPause={() => this.onPause(onPause)}
-                        onLoadedData={() => this.onLoadedData(onLoadedData)}
-                        ref={refs => {
-                            this.videoElement = refs;
-                        }}
-                    >
-                        <source src={src} type="video/mp4" />
-
-                    </video>
-                }
-                {
-                    drawDetection && <canvas
-                        style={{ "position": "absolute" }}
-                        hidden={false}
-                        ref={refs => {
-                            this.canvasElement = refs;
-                        }}
-                    />
-                }
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 VideoFaces.defaultProps = {
